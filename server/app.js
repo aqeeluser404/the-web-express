b1609f18a7f7b5a67db35f2fc6c971e2760beae4
Aqeel <106826671+aqeeluser404@users.noreply.github.com>
2025-01-17 10:26:12 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -73,17 +73,17 @@ app.get('/get-token', (req, res) => {
     }
 });
 // Route to remove the token (clear cookie)
-// app.post('/remove-token', (req, res) => {
-//     const isProduction = process.env.NODE_ENV === 'production';
-//     res.clearCookie('token', { httpOnly: true, secure: isProduction, sameSite: 'None', path: '/' });
-//     res.send({ message: 'Token removed' });
-// })
-// Route to remove the token (clear cookie)
 app.post('/remove-token', (req, res) => {
     const isProduction = process.env.NODE_ENV === 'production';
-    res.clearCookie('token', { httpOnly: true, secure: isProduction, sameSite: isProduction ? 'None' : 'Lax', path: '/' });
+    res.clearCookie('token', { httpOnly: true, secure: isProduction, sameSite: 'None', path: '/' });
     res.send({ message: 'Token removed' });
 })
+// Route to remove the token (clear cookie)
+// app.post('/remove-token', (req, res) => {
+//     const isProduction = process.env.NODE_ENV === 'production';
+//     res.clearCookie('token', { httpOnly: true, secure: isProduction, sameSite: isProduction ? 'None' : 'Lax', path: '/' });
+//     res.send({ message: 'Token removed' });
+// })
 
 
 // application routes
